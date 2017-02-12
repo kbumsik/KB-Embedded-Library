d01fb991b779a2ef289391c241ad919f543ef011
Bumsik Kim <kbumsik@gmail.com>
2017-02-12 03:57:09 -0500

    Fixed vagrant environment

--- scripts/setup/config_after_provisioning.sh
+++ scripts/setup/config_after_provisioning.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln -s ~/wolfieMouse ~/Desktop/  # TODO: make it universal
+ln -s ~/wolfieMouse ~/Desktop/
 sed ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml -e 's:"switch_window_key":"empty":g' -i
 xset s blank
