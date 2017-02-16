374429b2e340f8ae61007e5b28bac36481af8d44
Bumsik Kim <kbumsik@gmail.com>
2017-02-16 03:07:02 -0500

    Blinky project compiles fine

--- examples/01_Blinky/src/main.c
+++ examples/01_Blinky/src/main.c
@@ -1,9 +1,9 @@
-#include "main.h"
+#include "kb_module_config.h"
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal.h"
 #include "system_config.h"
-#include "trace.h"
-#include "kb_timer.h"
+#include "kb_trace.h"
+#include "kb_tick.h"
 
 int main(void)
 {
@@ -13,15 +13,15 @@ int main(void)
     // Initialize all configured peripherals
     peripheral_init();
 
-    HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
-    kb_timer_delay_ms(500);
-    HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+    HAL_GPIO_TogglePin(LED1_PORT, LED1_PIN);
+    kb_delay_ms(500);
+    HAL_GPIO_TogglePin(LED1_PORT, LED1_PIN);
     trace_puts("Hello ARM World!");
     uint32_t seconds = 0;
     while (1)
     {
-    	HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
-    	kb_timer_delay_us(1000000);
+    	HAL_GPIO_TogglePin(LED1_PORT, LED1_PIN);
+    	kb_delay_us(1000000);
 
         ++seconds;
         // Count seconds on the trace device.
